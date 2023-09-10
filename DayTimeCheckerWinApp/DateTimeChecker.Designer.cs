54c3748c405fbc6fee86ea5e8b2ec61cba4b2642
Le Hai <leehai1107@gmail.com>
2023-09-10 15:39:22 +0700

    date time checker app

---
+++ DayTimeCheckerWinApp/DateTimeChecker.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace DayTimeCheckerWinApp
+{
+    partial class DateTimeChecker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.txtMonth = new System.Windows.Forms.TextBox();
+            this.txtDay = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(236, 274);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(185, 23);
+            this.txtYear.TabIndex = 19;
+            // 
+            // txtMonth
+            // 
+            this.txtMonth.Location = new System.Drawing.Point(236, 224);
+            this.txtMonth.Name = "txtMonth";
+            this.txtMonth.Size = new System.Drawing.Size(185, 23);
+            this.txtMonth.TabIndex = 18;
+            // 
+            // txtDay
+            // 
+            this.txtDay.Location = new System.Drawing.Point(236, 172);
+            this.txtDay.Name = "txtDay";
+            this.txtDay.Size = new System.Drawing.Size(185, 23);
+            this.txtDay.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(157, 280);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 17);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Year";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(157, 230);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 17);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Month";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(160, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Day";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(150, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(309, 40);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Date Time Checker";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DayTimeCheckerWinApp.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(175, 102);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(346, 326);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 11;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(190, 326);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // DateTimeChecker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.txtMonth);
+            this.Controls.Add(this.txtDay);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnClear);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DateTimeChecker";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DateTimeChecker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DateTimeChecker_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtYear;
+        private TextBox txtMonth;
+        private TextBox txtDay;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Button btnCheck;
+        private Button btnClear;
+    }
+}
---
+++ DayTimeCheckerWinApp/DateTimeChecker.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace DayTimeCheckerWinApp
+{
+    public partial class DateTimeChecker : Form
+    {
+        byte day, month;
+        short year;
+        public DateTimeChecker()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtDay.Text = string.Empty;
+            txtMonth.Text = string.Empty;
+            txtYear.Text = string.Empty;
+        }
+
+
+        private void DateTimeChecker_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                DialogResult result = MessageBox.Show("Are you sure to exit?", "Confirm",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.No)
+                {
+                    e.Cancel = true; // Cancel the form closing event
+                }
+            }
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            byte.TryParse(txtDay.Text, out day);
+            byte.TryParse(txtMonth.Text, out month);
+            short.TryParse(txtYear.Text, out year);
+
+            if (isCorrectFormat(day, month, year))
+            {
+                if (isValidRange(day, month, year))
+                {
+                    if (IsValidDate(day, month, year) == true)
+                    {
+                        DialogResult dialog = MessageBox.Show($"{day}/{month}/{year} is correct date time!", "Message",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        DialogResult dialog = MessageBox.Show($"{day}/{month}/{year} is NOT correct date time!", "Message",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+
+        }
+
+        //check format
+        private bool isCorrectFormat(byte day, byte month, short year)
+        {
+
+            if (string.IsNullOrEmpty(txtDay.Text) || !byte.TryParse(txtDay.Text, out day))
+            {
+                DialogResult dialog = MessageBox.Show("Input data for Day is incorrect format!",
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            else if (string.IsNullOrEmpty(txtMonth.Text) || !byte.TryParse(txtMonth.Text, out month))
+            {
+                DialogResult dialog = MessageBox.Show("Input data for Month is incorrect format!",
+                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            else if (string.IsNullOrEmpty(txtYear.Text) || !short.TryParse(txtYear.Text, out year))
+            {
+                DialogResult dialog = MessageBox.Show("Input data for Year is incorrect format!",
+                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        //check textbox out of range
+        private bool isValidRange(byte day, byte month, short year)
+        {
+
+            if (day < 1 || day > 31)
+            {
+                DialogResult dialog = MessageBox.Show("Input data for Day is out of range!",
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            else if (month < 1 || month > 12)
+            {
+                DialogResult dialog = MessageBox.Show("Input data for Month is out of range!",
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            else if (year < 1000 || year > 3000)
+            {
+                DialogResult dialog = MessageBox.Show("Input data for Year is out of range!",
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+
+        }
+
+        //check Day in Month
+        byte DayInMonth(short Year, byte Month)
+        {
+            byte Day = 0;
+            if (Month == 1 || Month == 3 || Month == 5 || Month == 8 || Month == 7 || Month == 10
+                || Month == 12)
+            {
+                Day = 31;
+            }
+            else if (Month == 4 || Month == 6 || Month == 9 || Month == 11)
+            {
+                Day = 30;
+            }
+            else if (Month == 2)
+            {
+                if ((Year % 400) == 0)
+                {
+                    Day = 29;
+                }
+                else if ((Year % 100) == 0)
+                {
+                    Day = 28;
+                }
+                else if ((Year % 4) == 0)
+                {
+                    Day = 29;
+                }
+                else
+                {
+                    Day = 28;
+                }
+            }
+            return Day;
+        }
+
+            //check valid date
+            private bool IsValidDate(byte Day, byte Month, short Year)
+        {
+            if (Month >= 1 && Month <= 12)
+            {
+                if (Day >= 1)
+                {
+                    if (Day <= DayInMonth(Year, Month))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
