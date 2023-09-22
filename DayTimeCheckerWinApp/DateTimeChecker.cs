f8490e2a115cb05bb7d481d657528259772b802a
leehai1107 <leehai1107@gmail.com>
2023-09-22 09:41:23 +0700

    commit changed

--- DayTimeCheckerWinApp/DateTimeChecker.cs
+++ DayTimeCheckerWinApp/DateTimeChecker.cs
@@ -65,6 +65,14 @@ namespace DayTimeCheckerWinApp
             }
 
         }
+        //this is for test
+        /// <summary>
+        /// /?????????????????????????????///********88
+        /// </summary>
+        /// <param name="day"></param>
+        /// <param name="month"></param>
+        /// <param name="year"></param>
+        /// <returns></returns>
 
         //check format
         private bool isCorrectFormat(byte day, byte month, short year)
