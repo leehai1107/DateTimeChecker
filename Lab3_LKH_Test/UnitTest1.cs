72a2168a3ddf93f4d5c5bd008ef5651ccfab6360
HoangKim0312 <70700909+HoangKim0312@users.noreply.github.com>
2023-10-07 08:28:04 +0700

    hoang test case
    hoang test case

---
+++ Lab3_LKH_Test/UnitTest1.cs
@@ -0,0 +1,25 @@
+using DayTimeCheckerWinApp;
+
+namespace Lab3_LKH_Test
+{
+    public class Tests
+    {
+        [TestFixture]
+        public class DateTimeCheckerTest
+        {
+            
+        }   
+        DateTimeChecker dateTimeChecker;
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
