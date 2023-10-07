1f0aa4bf4b39e4079abaca822124a758b1d54555
leehai1107 <leehai1107@gmail.com>
2023-10-07 07:46:49 +0700

    add Hai test case

--- TestProject/HaiTest.cs
+++ TestProject/HaiTest.cs
@@ -12,9 +12,33 @@ namespace TestProject
         }
 
         [Test]
-        public void Test1()
+        public void UTCID13()
         {
-            Assert.AreEqual(28,dateTimeChecker.DayInMonth(2023,2));
+            Assert.AreEqual(false,dateTimeChecker.IsValidDate(31,6,2000));
+        }
+
+        [Test]
+        public void UTCID14()
+        {
+            Assert.AreEqual(true, dateTimeChecker.IsValidDate(30, 6, 2000));
+        }
+
+        [Test]
+        public void UTCID15()
+        {
+            Assert.AreEqual(false, dateTimeChecker.IsValidDate(31, 9, 2020));
+        }
+
+        [Test]
+        public void UTCID13_DayInMonth()
+        {
+            Assert.AreEqual(30, dateTimeChecker.DayInMonth(2021, 11));
+        }
+        
+        [Test]
+        public void UTCID14_DayInMonth()
+        {
+            Assert.AreEqual(31, dateTimeChecker.DayInMonth(2010, 12));
         }
     }
 }
